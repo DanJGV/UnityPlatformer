c5e5c281d343328878e63c9604e053985954a4f1
DanJGV <dangv93@gmail.com>
2020-09-07 18:07:35 -0500

    Unity Platformer

---
+++ Code/Platformer/Assets/RaycastTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class RaycastTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray r = new Ray(transform.position, Vector3.down);
+        Debug.DrawLine(r.origin, r.direction, Color.red, 100.0f);
+        Debug.Log("Updating");
+    }
+}
